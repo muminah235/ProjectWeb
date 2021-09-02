c086bd5f4ccf14b1f5a5dabbc2371712340f1962
Muminah <6110210235@psu.ac.th>
2021-09-02 21:30:09 +0700

    ค้นหา

--- cs-phamacy/src/components/Navbar.js
+++ cs-phamacy/src/components/Navbar.js
@@ -1,15 +1,50 @@
+import { useEffect, useState } from 'react';
+import Axios from 'axios'
+
 export default function Navbar() {
+
+    const [SearchText, setSearchText] = useState('');
+    const [SeachStatus, setSearchStatus] = useState('');
+
+    useEffect(()=>{
+        localStorage.setItem('seachtext', JSON.stringify(SearchText));
+    })
+
+    const seachtext = (e) => {
+        e.preventDefault();
+        Axios.post('http://localhost:4002/search',{
+            seachtext: Product_name
+        }).then((response) => {
+            if(response.data.message){
+              setSeachStatus(response.data.message)
+            }else{
+              e.preventDefault();
+              setSearchStatus(response.data[0].Product_name)
+              alert("ไม่พบยาที่ต้องการ");
+              e.preventDefault();
+            }
+
+            })
+    }
+
+
     return (
         <div>
             <nav class = "navbar navbar-expand-lg navbar bg-light">
                 
                     
-                    
                     <ul class="navbar-nav">
                     <a class = "navbar-brand" href="/">CS Pharmacy</a>
                     <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="ค้นหายา" aria-label="Search"></input>
-                        <button class="btn btn-outline-success" type="submit">ค้นหา</button>
+                    <input 
+                        class="form-control me-2" 
+                        type="search" 
+                        placeholder="ค้นหายา" 
+                        aria-label="Search"
+                        value={SearchText} 
+                        onChange={(event) => { setSearchText(event.target.value) }}
+                    />
+                        <button class="btn btn-outline-success" type="submit" onClick={seachtext}>ค้นหา</button>
                     </form>
 
                         <li class="nav-item">
